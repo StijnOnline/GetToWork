ba50c1a6db0cbbd90628b8d37b5fd8a34a2babf4
Stijn van Deijzen <stijnvandeijzen@hotmail.nl>
2020-04-25 20:55:14 +0200

    Grabable Bike Handles

---
+++ GetToWorkUnity/Assets/Project/Scripts/Handle.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Interactable))]
+public class Handle: MonoBehaviour {
+    private List<Hand> holdingHands = new List<Hand>();
+    public Rigidbody steerRigidbody;
+    public GameObject fakeHand;
+
+    [EnumFlags]
+    public Hand.AttachmentFlags attachmentFlags = 0;
+    public float attachForce;
+    public float attachForceDamper;
+    
+
+    private void Start() {
+        fakeHand.SetActive(false);
+    }
+
+    void Update() {
+        for(int i = 0; i < holdingHands.Count; i++) {
+            if(holdingHands[i].IsGrabEnding(this.gameObject)) {
+                Detach(holdingHands[i]);
+            }
+        }
+    }
+
+    private void HandHoverUpdate(Hand hand) {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+        if(startingGrabType != GrabTypes.None) {
+            Attach(hand, startingGrabType);
+        }
+    }
+
+    private void Attach(Hand hand, GrabTypes startingGrabType) {
+        Detach(hand);
+        hand.AttachObject(this.gameObject, startingGrabType, attachmentFlags);
+        // Don't let the hand interact with other things while it's holding us
+        hand.HoverLock(null);
+        holdingHands.Add(hand);
+        fakeHand.SetActive(true);
+    }
+
+    private void Detach(Hand hand) {
+        int i = holdingHands.IndexOf(hand);
+        if(i != -1) {
+            // Allow the hand to do other things
+            holdingHands[i].HoverUnlock(null);
+            holdingHands[i].DetachObject(this.gameObject, false);
+            Util.FastRemove(holdingHands, i);
+            fakeHand.SetActive(false);
+        }
+    }
+
+    void FixedUpdate() {
+        for(int i = 0; i < holdingHands.Count; i++) {
+            Vector3 vdisplacement = holdingHands[i].transform.position - transform.position;
+            steerRigidbody.AddForceAtPosition(attachForce * vdisplacement, transform.position, ForceMode.Acceleration);
+            steerRigidbody.AddForceAtPosition(-attachForceDamper * steerRigidbody.GetPointVelocity(transform.position), transform.position, ForceMode.Acceleration);
+        }
+
+    }
+}
---
+++ GetToWorkUnity/Assets/StijnUtilityScripts/Editor/CenterChildrenToolWindow.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+class CenterChildrenToolWindow : EditorWindow {
+    /*
+    private Transform parentObject;
+    private Transform childObject;
+    private Method method;
+
+    enum Method { Center, Child }
+
+    [MenuItem("Window/Center Children")]
+    static void Init() {
+        CenterChildrenToolWindow window = (CenterChildrenToolWindow)EditorWindow.GetWindow(typeof(CenterChildrenToolWindow));
+    }
+
+    void OnGUI() {
+        //Parent Object Field
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Parent Object");
+        parentObject = (Transform)EditorGUILayout.ObjectField(parentObject, typeof(Transform));
+        if(GUILayout.Button("Selected")) {
+            parentObject = Selection.activeTransform;
+        }
+        EditorGUILayout.EndHorizontal();
+
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Target Position");
+        method = (Method)EditorGUILayout.EnumPopup(method);
+        EditorGUILayout.EndHorizontal();
+
+        EditorSeparator(5, 2);
+
+
+        switch(method) {
+            case Method.Center:
+
+                GUILayout.Label("Moves parent to center of children");
+                if(GUILayout.Button("Center")) {
+                    Center(parentObject, ChildAveragePosition(parentObject));
+                }
+                break;
+            case Method.Child:
+                GUILayout.Label("Moves parent to child");
+                //Child Object Field
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.Label("Child Object");
+                childObject = (Transform)EditorGUILayout.ObjectField(childObject, typeof(Transform));
+                if(GUILayout.Button("Selected")) {
+                    childObject = Selection.activeTransform;
+                }
+                EditorGUILayout.EndHorizontal();
+
+                if(GUILayout.Button("Center")) {
+                    Center(parentObject, childObject.localPosition);
+                }
+                break;
+        }
+
+    }
+    
+         public static void EditorSeparator(float padding, float height) {
+        GUILayout.Space(padding);
+        EditorGUI.DrawRect(EditorGUILayout.GetControlRect(false, height), Color.grey);
+        GUILayout.Space(padding);
+    }
+         */
+
+    public static void Center(Transform parentObject, Vector3 referencePosition) {
+        for(int i = 0; i < parentObject.childCount; i++) {
+            Transform child = parentObject.GetChild(i);
+            Undo.RecordObject(child, "Centered");
+            child.localPosition -= referencePosition;
+        }
+        Undo.RecordObject(parentObject, "Centered");
+        parentObject.position += referencePosition;
+
+    }
+
+    public static Vector3 ChildAveragePosition(Transform parent) {
+        Vector3 referencePosition = Vector3.zero;
+        foreach(Transform child in parent) {
+            referencePosition += child.localPosition;
+        }
+        return (referencePosition / parent.childCount);
+    }
+
+    [MenuItem("Tools/Center Container")]
+    public static void SimpleCenter() {
+        Center(Selection.activeTransform, ChildAveragePosition(Selection.activeTransform));
+    }
+}
