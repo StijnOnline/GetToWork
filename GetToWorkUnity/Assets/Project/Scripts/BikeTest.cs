37a315e58ad30116d55bc5ef401bd15557b701a9
Stijn van Deijzen <stijnvandeijzen@hotmail.nl>
2020-05-11 16:24:49 +0200

    OLD Bike Test Scripts

---
+++ GetToWorkUnity/Assets/Project/Scripts/BikeTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BikeTest : MonoBehaviour {
+    public float steerValue = 0;
+    public float steerSpeed = 1;
+    public float speed = 1;
+    public float wheelRadius = 1;
+    //private Rigidbody rb;
+    public Transform wheel1;
+    public Transform wheel2;
+    public LayerMask bikeMask;
+
+    void Start() {
+        //rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        UpdateBike();
+    }
+
+    void UpdateBike() {
+        steerValue += Input.GetAxisRaw("Horizontal") / 100f * steerSpeed;
+        transform.Translate(speed * transform.forward);
+        transform.rotation *= Quaternion.AngleAxis(steerValue, transform.up); //steering
+
+        RaycastHit hit;
+        if(!Physics.Raycast(wheel1.position, -wheel1.transform.up, out hit, 3f, bikeMask))
+            return;
+
+        if(!Physics.Raycast(wheel1.position, -hit.normal, out hit, 3f, bikeMask))
+            return;
+
+
+        Vector3 wheel1GroundPos = hit.point;
+        if(!Physics.Raycast(wheel2.position, -wheel2.transform.up, out hit, 3f, bikeMask))
+            return;
+        if(!Physics.Raycast(wheel2.position, -hit.normal, out hit, 3f, bikeMask))
+            return;
+
+
+
+        
+
+
+        Vector3 wheel2targetPos = hit.point + hit.normal * wheelRadius;
+        transform.position -= wheel2.position - wheel2targetPos;
+
+        
+    }
+}
---
+++ GetToWorkUnity/Assets/Project/Scripts/BikeTest2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class BikeTest2 : MonoBehaviour {
+
+    private Rigidbody rb;
+    private float steerValue;
+    public float steerSpeed;
+    public float balanceForce;
+    public float speed;
+    public WheelCollider frontWheel;
+    public WheelCollider backWheel;
+    public LayerMask isGround;
+    public LayerMask isWall;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = new Vector3(0f,2f,0f);
+    }
+
+    private void Update() {
+        steerValue += Input.GetAxisRaw("Horizontal") / 100f * steerSpeed;
+        //rb.MoveRotation( transform.rotation * Quaternion.AngleAxis(steerValue, transform.up)); //steering
+        frontWheel.steerAngle = steerValue;
+        backWheel.motorTorque = speed;
+
+        //rb.AddForce(transform.forward * speed);
+
+        rb.AddTorque(transform.forward * balanceForce * transform.rotation.z);
+
+        RaycastHit hit;
+        if(Physics.Raycast(rb.position, -transform.up, out hit, 3f, isGround)) {
+            //Vector3 projection = Vector3.ProjectOnPlane(transform.forward, hit.normal);
+            //Quaternion rotation = Quaternion.LookRotation(projection, hit.normal);
+            //rb.MoveRotation(Quaternion.Lerp(rb.rotation, rotation, Time.deltaTime * 5f));
+            //rb.AddTorque(transform.forward * balanceForce * Vector3.Dot( transform.up,hit.normal));
+        }
+        
+
+    }
+
+    /*void OnCollisionStay(Collision collision) {
+        //If the ship has collided with an object on the Wall layer...
+        if(collision.gameObject.layer == isWall) {
+            //...calculate how much upward impulse is generated and then push the vehicle down by that amount 
+            //to keep it stuck on the track (instead up popping up over the wall)
+            Vector3 upwardForceFromCollision = Vector3.Dot(collision.impulse, transform.up) * transform.up;
+            rb.AddForce(-upwardForceFromCollision, ForceMode.Impulse);
+        }
+    }*/
+
+}
