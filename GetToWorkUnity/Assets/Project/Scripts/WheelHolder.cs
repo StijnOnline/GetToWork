35d36d63c86fa8bab2c1821ea7404b05542de7b9
Stijn van Deijzen <stijnvandeijzen@hotmail.nl>
2020-05-11 16:23:44 +0200

    Unity Hover Bike

---
+++ GetToWorkUnity/Assets/Project/Scripts/WheelHolder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelHolder : MonoBehaviour
+{
+    public Transform wheel;
+    public LayerMask layerMask;
+    public float maxDist = 0.5f;
+    public float radius = 0.5f;
+    public float lerpVal = 0.2f;
+
+    
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position,transform.forward, out hit, maxDist, layerMask)) {
+            wheel.position = Vector3.Lerp(wheel.position, hit.point + hit.normal * radius, lerpVal);
+        } else {
+            wheel.position = Vector3.Lerp(wheel.position, transform.position + transform.forward * maxDist,lerpVal);
+        }
+    }
+}
