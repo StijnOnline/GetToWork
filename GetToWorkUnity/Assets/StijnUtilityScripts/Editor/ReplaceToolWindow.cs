a17a57fb608c0bc9ef14a6def3ba0a5c2c20686f
Stijn van Deijzen <stijnvandeijzen@hotmail.nl>
2020-05-23 19:25:18 +0200

    Added other Replace Method in tool

--- GetToWorkUnity/Assets/StijnUtilityScripts/Editor/ReplaceToolWindow.cs
+++ GetToWorkUnity/Assets/StijnUtilityScripts/Editor/ReplaceToolWindow.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 
 public class ReplaceToolWindow : EditorWindow {
 
+    
     private GameObject targetObject = null;
     private Vector3 positionOffset = Vector3.zero;
     private Vector3 rotationOffset = Vector3.zero;
     private float scaleMultiplier = 1f;
-    private string responseText;
+
+    private GameObject originalObject = null;
+    private GameObject replacedObject = null;
 
     [MenuItem("Window/Replace Selection")]
     static void Init() {
@@ -18,45 +21,63 @@ public class ReplaceToolWindow : EditorWindow {
     }
 
     void OnGUI() {
+
+
+        GUILayout.Label("Simple Replace", EditorStyles.boldLabel);
         targetObject = (GameObject)EditorGUILayout.ObjectField(targetObject, typeof(GameObject));
         positionOffset = EditorGUILayout.Vector3Field("Position Offset", positionOffset);
         rotationOffset = EditorGUILayout.Vector3Field("Rotation Offset", rotationOffset);
         scaleMultiplier = EditorGUILayout.FloatField("Scale Multiplier", scaleMultiplier);
 
         if(GUILayout.Button("Replace Selection")) {
-            responseText = Replace();
-
-            ResetResponseText();
+            SimpleReplace();
         }
-        GUILayout.Label(responseText);
+
+        GUILayout.Space(20);
+        GUILayout.Label("Copy Replace", EditorStyles.boldLabel);
+        originalObject = (GameObject)EditorGUILayout.ObjectField("Original Object", originalObject, typeof(GameObject));
+        replacedObject = (GameObject)EditorGUILayout.ObjectField("Replaced Object", replacedObject, typeof(GameObject));
+
+        if(GUILayout.Button("Replace Selection")) {
+            CopyReplace();
+        }
     }
 
-    string Replace() {
+    void SimpleReplace() {
         if(targetObject == null)
-            return "Target object not set correctly";
+            return;
         if(Selection.transforms.Length == 0)
-            return "No objects Selected";
+            return;
 
         foreach(GameObject referenceObject in Selection.gameObjects) {
             //Create New
-
-            Vector3 globalPostionOffset = Vector3.zero;
-            globalPostionOffset += referenceObject.transform.forward * positionOffset.z;
-            globalPostionOffset += referenceObject.transform.right * positionOffset.x;
-            globalPostionOffset += referenceObject.transform.up * positionOffset.y;
-
+            Vector3 globalPostionOffset = referenceObject.transform.TransformDirection(positionOffset);
             GameObject created = Instantiate(targetObject,referenceObject.transform.position + globalPostionOffset, referenceObject.transform.rotation * Quaternion.Euler(rotationOffset));
             created.transform.localScale *= scaleMultiplier;
             Undo.RegisterCreatedObjectUndo(created,"Created Replacement");
-
             Undo.DestroyObjectImmediate(referenceObject);
         }
-
-        return "Objects Replaced";
     }
 
-    async void ResetResponseText() {
-        await Task.Delay(600);
-        responseText = "";
+    void CopyReplace() {
+        if(originalObject == null)
+            return;
+        if(replacedObject == null)
+            return;
+        if(Selection.transforms.Length == 0)
+            return;
+
+        float relativeScale = replacedObject.transform.lossyScale.magnitude / originalObject.transform.lossyScale.magnitude;
+        Quaternion relativeRotation = Quaternion.RotateTowards(originalObject.transform.rotation, replacedObject.transform.rotation, float.MaxValue);
+        Vector3 relativePos = replacedObject.transform.position - originalObject.transform.transform.position;        
+
+        foreach(GameObject referenceObject in Selection.gameObjects) {
+            //Create New
+            Vector3 globalPostionOffset = replacedObject.transform.InverseTransformDirection(relativePos);
+            GameObject created = Instantiate(replacedObject, referenceObject.transform.position + globalPostionOffset, referenceObject.transform.rotation * relativeRotation);
+            created.transform.localScale *= relativeScale;
+            Undo.RegisterCreatedObjectUndo(created, "Created Replacement");
+            Undo.DestroyObjectImmediate(referenceObject);
+        }
     }
 }
