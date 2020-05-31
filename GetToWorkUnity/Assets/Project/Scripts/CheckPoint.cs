7910296dbe0ddee5e5518b2bbf4ae7254d8a2ea7
Stijn van Deijzen <stijnvandeijzen@hotmail.nl>
2020-05-31 16:21:38 +0200

    CheckPoint unityevent

--- GetToWorkUnity/Assets/Project/Scripts/CheckPoint.cs
+++ GetToWorkUnity/Assets/Project/Scripts/CheckPoint.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Events;
 
 public class CheckPoint : MonoBehaviour {
 
     public Transform spawnPoint;
+    public UnityEvent unityEvent;
     
     private bool reached = false;
 
@@ -11,8 +13,7 @@ public class CheckPoint : MonoBehaviour {
         if(!reached && other.CompareTag(GameManager.Instance.playerTag)) {
             reached = true;
             GameManager.Instance.CheckPointReached(this);
-
-            //particles and audio or something
+            unityEvent.Invoke();
         }
     }
 }
