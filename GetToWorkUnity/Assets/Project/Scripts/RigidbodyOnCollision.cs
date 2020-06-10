8ea5dc0b59dc7b7a70ea12324fa3ee5d2920ace3
Stijn van Deijzen <stijnvandeijzen@hotmail.nl>
2020-06-10 13:01:32 +0200

    Rigidbody enable Script

---
+++ GetToWorkUnity/Assets/Project/Scripts/RigidbodyOnCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RigidbodyOnCollision : MonoBehaviour
+{
+    private Rigidbody rb;
+    private LayerMask requireLayer;
+    private string requireTag;
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.CompareTag(requireTag) && ((1<<collision.gameObject.layer & requireLayer.value) != 0)) {
+
+            rb.isKinematic = false;
+
+        }
+
+    }
+}
